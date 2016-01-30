51eac510e1ec53143d7849f36d2e6d96e6719548
youngliu323 <youngliu323@gmail.com>
2016-01-30 12:44:12 -0600

    Deleted unnecessary file

--- FermataBeta/FermataBeta/SongTableViewController.swift
+++ FermataBeta/FermataBeta/SongTableViewController.swift
@@ -12,7 +12,6 @@ import WebKit
 
 class SongTableViewController: UITableViewController {
     
-    
     @IBOutlet var Menu: UIBarButtonItem!
     
     //MARK: Properties
@@ -105,30 +104,4 @@ class SongTableViewController: UITableViewController {
         }
     }
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 }
