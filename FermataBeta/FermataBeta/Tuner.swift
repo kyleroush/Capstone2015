f32284d35319c95bee73eb0e86206ea273f3becf
youngliu323 <youngliu323@gmail.com>
2016-02-10 20:18:09 -0600

    Finished half of the tuner (functionality)

--- FermataBeta/FermataBeta/Tuner.swift
+++ FermataBeta/FermataBeta/Tuner.swift
@@ -2,11 +2,9 @@
 //  Tuner.swift
 //  FermataBeta
 //
-//  Created by Young Liu on 1/30/16.
+//  Created by Young Liu and Eric on 1/30/16.
 //  Copyright © 2016 Young Liu. All rights reserved.
 //
-
-//Eric and Sam: work on this file
 
 //ALL the free notes come from this page
 
@@ -16,29 +14,36 @@ import AVFoundation
 class Tuner: UIViewController {
    
     var audioPlayer = AVAudioPlayer()
+    var count = 0
     
     var noteList = [String]()
     
-
-    
-    
-    @IBAction func play(sender: AnyObject) {
-        
-        audioPlayer.prepareToPlay()
-        audioPlayer.play()
-        
+    @IBAction func UpButton(sender: AnyObject) {
+        if count == noteList.count-1 {
+            count = 0
+        }
+        else {
+           count++
+        }
+        print(count)
+        Note.text = noteList[count]
     }
     
-
+    @IBAction func DownButton(sender: AnyObject) {
+        if count == 0{
+            count=noteList.count-1
+        }
+        else {
+            count--
+        }
+        print(count)
+        Note.text = noteList[count]
+    }
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        noteList = ["0A", "10G", "11Gs", "1As", "2B", "3C", "4Cs", "5D", "6Ds","7E","8F","9Fs"]
-        
-        let alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("0A", ofType: "wav")!)
+    @IBOutlet var Note: UILabel!
+    //play button: plays sound
+    @IBAction func play(sender: AnyObject) {
+        let alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(noteList[count], ofType: "wav")!)
         print(alertSound)
         
         // Removed deprecated use of AVAudioSessionDelegate protocol
@@ -46,10 +51,31 @@ class Tuner: UIViewController {
         do
         {
             
-        try audioPlayer = AVAudioPlayer(contentsOfURL: alertSound)
+            try audioPlayer = AVAudioPlayer(contentsOfURL: alertSound)
         }
         catch _ { }
         
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+        print(count)
+        
+    }
+    
+
+    //stops the music
+    @IBAction func stopButton(sender: AnyObject) {
+        audioPlayer.stop()
+    }
+    
+    @IBOutlet var NotePlayed: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        noteList = ["0A", "10G", "11Gs", "1As", "2B", "3C", "4Cs", "5D", "6Ds","7E","8F","9Fs"]
+        
+        Note.text = noteList[count]
         
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
     }
